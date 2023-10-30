766ce561449d1b65b9cb096804a63e07ed31720f
bassim25 <144334232+bassim25@users.noreply.github.com>
2023-10-30 03:37:06 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PlayersList from './PlayersFIFAList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Player Cards</h1>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ players.js
@@ -0,0 +1,34 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "inter miami",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 36,
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "al nasr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 38,
+    },
+    {
+      name: "Neymar Jr.",
+      team: "al hilel",
+      nationality: "Brazil",
+      jerseyNumber: 10,
+      age: 31,
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 24,
+    },
+  ];
+  
+  export default players;
+  
+  
